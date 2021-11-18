ee036a062ac95d7f8ce9e02e1d0eec279f119043
PriteshKiri <77957844+PriteshKiri@users.noreply.github.com>
2021-11-18 13:42:40 +0530

    Update faq.js

--- faq.js
+++ faq.js
@@ -23,3 +23,28 @@ $(function () {
 
   var accordion = new Accordion($("#accordion"), false);
 });
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  var elmnt = document.getElementById("hubspot-form-scroll");
+  elmnt.scrollIntoView();
+}
